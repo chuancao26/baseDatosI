4aa55a1b5d2bac9459d777e2f97863ab4261d880
chuancao26 <chuancao@unsa.edu.pe>
2024-02-05 21:28:04 -0500

    Agregando formularios para representantate y cliente

--- app/src/app.py
+++ app/src/app.py
@@ -3,12 +3,19 @@ from flask_mysqldb import MySQL
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 app = Flask(__name__)
 
-@app.route('/login')
+@app.route('/')
 def login():
     return render_template('login.html')
 @app.route('/ClienteFormulario')
 def ClienteFormulario():
     return render_template("formCliente.html")
+@app.route('/EmpleadoFormulario')
+def EmpleadoFormulario():
+    return render_template("formEmpleado.html")
+@app.route('/representanteFormulario')
+def representanteFormulario():
+    return render_template("formRepresentante.html")
+
 
 
 if __name__ == '__main__':
