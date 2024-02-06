22bdb03590fb1d23562d35a7e11e551646717caa
chuancao26 <chuancao@unsa.edu.pe>
2024-02-05 20:35:12 -0500

    Agregando formularios, css y algunos html mas. Tambien se agrego el html layout.

--- app/src/app.py
+++ app/src/app.py
@@ -2,5 +2,16 @@ from flask import Flask, render_template, request, url_for, redirect, jsonify, f
 from flask_mysqldb import MySQL
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user
 app = Flask(__name__)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+@app.route('/ClienteFormulario')
+def ClienteFormulario():
+    return render_template("formCliente.html")
+
+
 if __name__ == '__main__':
-    app.run(debub=True)
+    app.run(debug=True)
+
+
